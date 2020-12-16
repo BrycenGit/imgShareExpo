0af36def67f4ac1b993ef15c4c778e99d249a78b
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-16 14:00:56 -0800

    add file sharing and anonymous file

--- App.js
+++ App.js
@@ -1,25 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import logo from './assets/logo.png'
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import uploadToAnonymousFilesAsync from 'anonymous-files'
+
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri })
+  };
+
+  let openShareDialogAsync = async () => {
+    if(!(await Sharing.isAvailableAsync())) {
+      alert(`uh oh, sharing isnt available on your platform`);
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image 
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Image source={imgURI} style={styles.logo} />
-      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
+      <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
 
-      <TouchableOpacity
-        onPress={() => alert("Hello, World!")}
-        style={styles.button}>
-          <Text style={styles.buttonText}>Pick a photo</Text>
-        </TouchableOpacity>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const imgURI = { uri: "https://i.imgur.com/TkIrScD.png" }
 
 const styles = StyleSheet.create({
   container: {
@@ -31,20 +73,26 @@ const styles = StyleSheet.create({
   logo: {
     width: 305,
     height: 159,
-    marginBottom: 10,
+    marginBottom: 20,
   },
   instructions: {
     color: '#888',
     fontSize: 18,
     marginHorizontal: 15,
+    marginBottom: 10,
   },
   button: {
-    backgroundColor: "blue",
+    backgroundColor: 'blue',
     padding: 20,
     borderRadius: 5,
   },
   buttonText: {
     fontSize: 20,
-    color: '#fff'
+    color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain'
   },
 });
