dd5abd8870f361278caaf601bcda933fa3542820
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-16 11:09:33 -0800

    add image to app

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={logo} style={imgSize} />
       <Text style={textStyle}>To share a photo from your phone with a friend, just press the button below!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const imgSize = { width: 305, height: 159 }
 
 const textStyle = {color: '#888', fontSize: 18}
 
